3dd373db220a529d8276d725db72296076b2037f
rina <k@rina.fyi>
2023-09-11 22:16:51 +1000

    add update checker.

--- update.sh
+++ update.sh
@@ -7,14 +7,23 @@
 
 set -e
 
+ARG="$1"
+do-upgrade() {
+  if [[ "$ARG" == '--upgrade' ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 PKGS=./pkgs.nix
 TMP=$(mktemp)
 
 curl() {
   if ! [[ -z "$GITHUB_TOKEN" ]]; then
-    command curl --header "Authorization: Bearer $GITHUB_TOKEN" "$@"
+    command curl -s --header "Authorization: Bearer $GITHUB_TOKEN" "$@"
   else
-    command curl "$@"
+    command curl -s "$@"
   fi
 }
 
@@ -32,13 +41,36 @@ update-github() {
   COMMITS_API="https://api.github.com/repos/$repo/commits/$branch"
   latest=$(curl "$COMMITS_API" | jq -r .sha)
 
-  nix-update -f "$PKGS" $attr --version $latest --commit --build
+  if do-upgrade; then
+    nix-update -f "$PKGS" $attr --version $latest --commit --build
+  else
+    current=$(nix-instantiate --eval -E "(import ./$PKGS {}).$attr.src.rev" | jq -r)
+    COMPARE="https://api.github.com/repos/$repo/compare/$current...$branch"
+    compare=$(curl "$COMPARE")
+    
+    result=$(echo "$compare" | jq '{ html_url, status, ahead_by, behind_by, total_commits }')
+    echo "$compare" | jq '{ html_url, status, ahead_by, behind_by, total_commits }' 
+
+    if [[ $(echo "$compare" | jq .total_commits) != 0 ]]; then
+      echo "::warning title=Package outdated: $attr::$attr differs by $(echo "$compare" | jq .total_commits) commits ($(echo "$compare" | jq .html_url -r))"
+    else
+      echo "::notice title=Package current: $attr::$attr differs by $(echo "$compare" | jq .total_commits) commits ($(echo "$compare" | jq .html_url -r))"
+    fi
+  fi
 }
 
 test-build() {
   pkg="$1"
-  nix-build $PKGS -A $pkg --no-out-link
+  if do-upgrade; then
+    nix-build $PKGS -A $pkg --no-out-link
+  fi
 }
+
+if do-upgrade; then
+  echo "Performing upgrade..."
+else
+  echo "Checking for upstream updates..."
+fi
 
 update-github asli UQ-PAC/aslp
 test-build aslp
