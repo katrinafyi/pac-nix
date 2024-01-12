b5b1039b41d1ab5c86bf788b1344fe87ce905b0c
rina <k@rina.fyi>
2024-01-12 11:37:17 +1000

    fix update.nix

--- update.nix
+++ update.nix
@@ -1,25 +1,32 @@
-{ runCommand
+{ lib
+, stdenv
+, runCommand
+, makeWrapper
 , python3
-, cacert
 , git
-, nix
+  # , nix
 , nix-update
 }:
 
-runCommand 
-"pac-nix-update" 
-{
-  buildInputs = [
-    python3
-    cacert
-    git
-    nix
-    nix-update
-  ];
+let path = lib.makeBinPath [ git nix-update ];
+in stdenv.mkDerivation {
+  pname = "pac-nix-update";
+  version = "0.1.0";
+  buildInputs = [ python3 makeWrapper ];
+
+  src = ./update.py;
+  dontUnpack = true;
 
   meta.mainProgram = "pac-nix-update";
-} 
-''
-mkdir -p $out/bin
-cp ${./update.py} $out/bin/pac-nix-update
-''
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -v $src $out/bin/pac-nix-update
+    wrapProgram $out/bin/pac-nix-update \
+      --suffix PATH : ${path}
+
+    runHook postInstall
+  '';
+}
