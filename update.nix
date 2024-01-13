9c32e9cd684ff8b14e7ab0a72e78740ba68e6eba
rina <k@rina.fyi>
2024-01-13 15:44:21 +1000

    use writePython3Bin for update.nix

--- update.nix
+++ update.nix
@@ -1,32 +1,8 @@
-{ lib
-, stdenv
-, runCommand
-, makeWrapper
-, python3
-, git
-  # , nix
+{ writers
+, nix
 , nix-update
 }:
 
-let path = lib.makeBinPath [ git nix-update ];
-in stdenv.mkDerivation {
-  pname = "pac-nix-update";
-  version = "0.1.0";
-  buildInputs = [ python3 makeWrapper ];
-
-  src = ./update.py;
-  dontUnpack = true;
-
-  meta.mainProgram = "pac-nix-update";
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin
-    cp -v $src $out/bin/pac-nix-update
-    wrapProgram $out/bin/pac-nix-update \
-      --suffix PATH : ${path}
-
-    runHook postInstall
-  '';
-}
+writers.writePython3Bin "pac-nix-update"
+{ libraries = [ nix nix-update ]; flakeIgnore = [ "E" "W" ]; }
+  (builtins.readFile ./update.py)
