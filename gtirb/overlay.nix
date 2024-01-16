73924bb7d0b71de4bb6e173a010756016115ec84
rina <k@rina.fyi>
2024-01-16 10:48:46 +1000

    rename gtirb-semantics.

--- gtirb/overlay.nix
+++ gtirb/overlay.nix
@@ -3,7 +3,7 @@ final: prev:
   ddisasm = prev.callPackage ./ddisasm.nix { };
   gtirb = prev.callPackage ./gtirb.nix { };
   gtirb-pprinter = prev.callPackage ./gtirb-pprinter.nix { };
-  capstone-grammatech = prev.callPackage ./capstone.nix { };
+  capstone-grammatech = prev.callPackage ./capstone-grammatech.nix { };
 
   libehp = prev.callPackage ./libehp.nix { };
   ocaml-hexstring = prev.callPackage ./ocaml-hexstring.nix { };
