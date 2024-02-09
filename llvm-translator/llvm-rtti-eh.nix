9168f32524957e16801b51e5a9b0f52e35d926b5
rina <k@rina.fyi>
2024-02-09 12:06:22 +1000

    llvm-rtti-eh: install Target includes as well.

--- llvm-translator/llvm-rtti-eh.nix
+++ llvm-translator/llvm-rtti-eh.nix
@@ -7,4 +7,11 @@ llvmPackages.libllvm.overrideAttrs (final: prev: {
     "-DLLVM_TARGETS_TO_BUILD=X86;AArch64;ARM"
   ];
   doCheck = false;
+
+  # install Target .inc files from build directory for lifter project.
+  postFixup = ''
+    cd /build/$sourceRoot/build/lib && file Target
+    find Target -name '*.inc' -print0 \
+      | xargs -0 -I{} cp -v --no-clobber --parents {} $dev/include
+  '';
 })
