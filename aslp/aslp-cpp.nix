de38f2f9cf300399224e883d5ee3bd12022601f6
rina <k@rina.fyi>
2024-02-19 18:46:41 +1000

    aslp-cpp: touch path [skip ci]

--- aslp/aslp-cpp.nix
+++ aslp/aslp-cpp.nix
@@ -1,5 +1,4 @@
 { stdenv
-, lib
 , cmake
 , asli
 }:
@@ -8,9 +7,7 @@ stdenv.mkDerivation {
   pname = "aslp-cpp";
   version = asli.version;
 
-  src = asli.src;
-  unpackPhase = "cp -r --no-preserve=mode $src source";
-  sourceRoot = "source/aslp-cpp";
+  src = "${asli.src}/aslp-cpp";
 
   patches = [ ];
 
