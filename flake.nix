7677769e8ca1fdaf7ca66383ba51e7d2e756c77b
rina <k@rina.fyi>
2024-03-01 12:19:59 +1000

    flake.nix: prepare for nix-patcher

--- flake.nix
+++ flake.nix
@@ -2,7 +2,11 @@
   nixConfig.extra-substituters = [ "https://pac-nix.cachix.org/" ];
   nixConfig.extra-trusted-public-keys = [ "pac-nix.cachix.org-1:l29Pc2zYR5yZyfSzk1v17uEZkhEw0gI4cXuOIsxIGpc=" ];
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs-upstream.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:katrinafyi/nixpkgs/pac-nix";
+
+  inputs.nixpkgs-patch-1.url = "https://github.com/NixOS/nixpkgs/compare/ffacc011dffba16ca360028d1f81cae99ff1280f..9a9cf8661391f21f7a44dc4823f815524351c94f.patch";
+  inputs.nixpkgs-patch-1.flake = false; 
 
   outputs = { self, nixpkgs, ... }:
     let
@@ -14,26 +18,11 @@
         "aarch64-linux"
       ];
 
-      patches = fetchpatch: [
-        (fetchpatch {
-          # https://nixpk.gs/pr-tracker.html?pr=285786
-          url = "https://github.com/NixOS/nixpkgs/compare/ffacc011dffba16ca360028d1f81cae99ff1280f..9a9cf8661391f21f7a44dc4823f815524351c94f.patch";
-          hash = "sha256-/1qoaHzROV5r1+1XfBPxgUdlMwEEXprp9O75/onUZAU=";
-        })
-      ];
+      patches = fetchpatch: [ ];
 
       nixpkgss = lib.genAttrs systems
         (system:
-          let
-            # HACK! this is very broken, remove as soon as PR is upstreamed.  
-            pkgs = nixpkgs.legacyPackages.${builtins.currentSystem or system};
-            nixpkgs' = pkgs.applyPatches {
-              name = "nixpkgs-patched";
-              src = nixpkgs;
-              patches = patches pkgs.fetchpatch;
-            };
-          in
-          import nixpkgs' {
+          import nixpkgs {
             system = system;
             overlays = [ overlay ];
           }
