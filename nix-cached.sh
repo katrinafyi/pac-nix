d33d61695c1eb3edc4f48b4a491a6288a59d8e9f
rina <k@rina.fyi>
2024-08-08 16:21:15 +1000

    fix nix-cached.sh with non-build commands

--- nix-cached.sh
+++ nix-cached.sh
@@ -2,6 +2,10 @@
 
 # if given command is fully cached, does nothing. otherwise, runs command.
 
-if [[ "$1" == build ]] && nix "$@" --dry-run 2>&1 | tee /dev/stderr | grep -q "will be built"; then
+if [[ "$1" == build ]]; then
+  if nix "$@" --dry-run 2>&1 | tee /dev/stderr | grep -q "will be built"; then
+    nix "$@"
+  fi
+else
   nix "$@"
 fi
