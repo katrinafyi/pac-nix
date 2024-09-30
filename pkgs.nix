4f51cc3e3c653410b397a16cf0042ba07c83667c
rina <k@rina.fyi>
2024-09-30 10:56:13 +1000

    pkgs.nix: get nixpkgs from flake.lock

--- pkgs.nix
+++ pkgs.nix
@@ -1,2 +1,10 @@
 { system ? builtins.currentSystem, overlays ? [ ] }:
-import <nixpkgs> { inherit system; overlays = overlays ++ [ (import ./overlay.nix) ]; }
+let
+  lockfile = builtins.fromJSON (builtins.readFile ./flake.lock);
+  locked = lockfile.nodes.nixpkgs.locked;
+  url =
+    if locked.type == "github"
+    then with locked; "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"
+    else builtins.throw "pac-nix: non-flake usage requires a locked GitHub repo as nixpkgs (got: ${builtins.toJSON locked})";
+  nixpkgs = builtins.fetchTarball url;
+in import nixpkgs { inherit system; overlays = overlays ++ [ (import ./overlay.nix) ]; }
