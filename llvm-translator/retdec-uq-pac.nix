8607c9dd47ee85080dfc11f98100ce0cffe332bb
rina <k@rina.fyi>
2024-12-02 14:45:35 +1000

    retdec-uq-pac: append patches

--- llvm-translator/retdec-uq-pac.nix
+++ llvm-translator/retdec-uq-pac.nix
@@ -2,7 +2,7 @@
 
 retdec.overrideAttrs (final: prev: {
   cmakeFlags = prev.cmakeFlags ++ [ "-DRETDEC_DEV_TOOLS=1" ];
-  patches = [
+  patches = prev.patches ++ [
     (fetchurl {
       url = "https://gist.githubusercontent.com/katrinafyi/c33f6f9ccaad4420f76f84e6cb219fe0/raw/0001-emit-names-for-capstone-intrinsics.patch";
       hash = "sha256-LJjzLw2R8ckB10jL92RH/kHggX2oQrAZNQUsonM6ciQ=";
