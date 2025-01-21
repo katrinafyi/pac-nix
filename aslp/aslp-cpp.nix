bee188fd0ff02d0da72a8889be1193de39774acd
rina <k@rina.fyi>
2025-01-21 21:33:21 +1000

    aslp-cpp formatting

--- aslp/aslp-cpp.nix
+++ aslp/aslp-cpp.nix
@@ -4,25 +4,24 @@
 , cmake
 }:
 
-let stdenv' = if stdenv.isDarwin then clang17Stdenv else stdenv;
-
-in let gh = fetchFromGitHub {
-  owner = "UQ-PAC";
-  repo = "aslp-rpc";
-  rev = "1a31d940e47246e24fb45cbc5614f24e425566ca";
-  hash = "sha256-o5+vNRIM0NslWc2NpgPuzdkVFlWmb6lMUGgNQvuNC60=";
-}; 
-
+let
+  stdenv' = if stdenv.isDarwin then clang17Stdenv else stdenv;
+  src = fetchFromGitHub {
+    owner = "UQ-PAC";
+    repo = "aslp-rpc";
+    rev = "1a31d940e47246e24fb45cbc5614f24e425566ca";
+    hash = "sha256-o5+vNRIM0NslWc2NpgPuzdkVFlWmb6lMUGgNQvuNC60=";
+  };
 in stdenv'.mkDerivation {
   pname = "aslp-cpp";
   version = "0.1";
 
-  src = "${gh}/aslp-client-http-cpp";
+  src = "${src}/aslp-client-http-cpp";
 
   patches = [ ];
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [ ];
 
-  cmakeFlags = [ "aslp-client-http-cpp" ];
+  cmakeFlags = [ ];
 }
