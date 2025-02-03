efc496732d81b289bbfa6a90c8058bb2b610aa3c
rina <k@rina.fyi>
2025-02-03 14:13:22 +1000

    aslp-cpp: migrate to aslp-rpc at 0-unstable-2025-02-03
    Diff: https://github.com/UQ-PAC/aslp-rpc/compare/4670a3c94b6dcaf2b0403b655e3f640b355ef578...12a5dfbda19429cc0c52f941ef67d184b227e3a4

--- aslp/aslp-cpp.nix
+++ aslp/aslp-cpp.nix
@@ -1,16 +1,25 @@
 { stdenv
+, fetchFromGitHub
 , clang17Stdenv
 , cmake
-, asli
 }:
 
-let stdenv' = if stdenv.isDarwin then clang17Stdenv else stdenv;
-
+let
+  stdenv' = if stdenv.isDarwin then clang17Stdenv else stdenv;
 in stdenv'.mkDerivation {
   pname = "aslp-cpp";
-  version = asli.version;
+  version = "0-unstable-2025-02-03";
 
-  src = "${asli.src}/aslp-cpp";
+  src = fetchFromGitHub {
+    owner = "UQ-PAC";
+    repo = "aslp-rpc";
+    rev = "12a5dfbda19429cc0c52f941ef67d184b227e3a4";
+    hash = "sha256-zsqdxE6HqqSZ86rMF32yTzUEz97mywSKrn3qndmxrDI=";
+  };
+
+  prePatch = ''
+    cd aslp-client-http-cpp
+  '';
 
   patches = [ ];
 
