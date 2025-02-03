2992179a6c283fcb2d2107562ae6c312a88fdc1c
rina <k@rina.fyi>
2025-02-03 15:32:06 +1000

    formatting changes

--- aslp/aslp_client_server_ocaml.nix
+++ aslp/aslp_client_server_ocaml.nix
@@ -1,11 +1,9 @@
 { lib
 , fetchFromGitHub
 , buildDunePackage
-  # pkgs
   # ocamlPackages
 , lwt
 , mtime
-  # for testing
 , aslp
 }:
 
@@ -16,33 +14,27 @@ buildDunePackage {
   minimalOCamlVersion = "5.0";
 
   src = fetchFromGitHub {
-  owner = "uq-pac";
-  repo = "aslp-rpc";
-  rev = "4670a3c94b6dcaf2b0403b655e3f640b355ef578";
-  hash = "sha256-hIELzXYr6uMWijeBZ6Rhip9JCronn2JLiJJsijzRjco=";
-  }
-  ;
+    owner = "UQ-PAC";
+    repo = "aslp-rpc";
+    rev = "4670a3c94b6dcaf2b0403b655e3f640b355ef578";
+    hash = "sha256-hIELzXYr6uMWijeBZ6Rhip9JCronn2JLiJJsijzRjco=";
+  };
+
   checkInputs = [ ];
   nativeCheckInputs = [ ];
-  buildInputs = [ aslp lwt mtime];
+  buildInputs = [ ];
   nativeBuildInputs = [ ];
   propagatedBuildInputs = [ aslp lwt mtime ];
 
   doCheck = true;
 
-  postPatch = ''
-    substituteInPlace aslp-server-http/bin/dune --replace-warn ' core ' ' '
-  '';
-
   outputs = [ "out" "dev" ];
 
-  passthru = {
-  };
+  passthru = { };
 
   meta = {
     homepage = "https://github.com/UQ-PAC/aslp-rpc";
-    description = "RPC connectors for aslp for OCaml and C++ (OCaml HTTP server component)";
+    description = "RPC connectors for aslp for OCaml and C++ (OCaml Unix socket server component)";
     maintainers = with lib.maintainers; [ katrinafyi ];
-    mainProgram = "aslp-server";
   };
 }
