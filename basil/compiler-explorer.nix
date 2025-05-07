68bf40a9ac83cb3ecfadf8124bf5b4d9bd0f9c5b
rina <k@rina.fyi>
2025-05-07 16:30:56 +1000

    compiler-explorer: 0-unstable-2024-07-30 -> 0-unstable-2025-05-01
    Diff: https://github.com/rina-forks/compiler-explorer/compare/d6a405a418097b2ef96145609a316c7f0c11a274...3c6c1fcfd1299b8fd1a739fb5949664053708dff

--- basil/compiler-explorer.nix
+++ basil/compiler-explorer.nix
@@ -11,7 +11,7 @@
 
 buildNpmPackage {
   pname = "compiler-explorer-ailrst";
-  version = "0-unstable-2024-07-30";
+  version = "0-unstable-2025-05-01";
 
   nativeBuildInputs = [ makeWrapper husky ];
 
@@ -20,8 +20,8 @@ buildNpmPackage {
   src = fetchFromGitHub {
     owner = "rina-forks";
     repo = "compiler-explorer";
-    rev = "d6a405a418097b2ef96145609a316c7f0c11a274";
-    sha256 = "sha256-+QNxpbVcZSOkLAmW3NZ37o6vtZhI4YyU/1eRtxPQrvk=";
+    rev = "3c6c1fcfd1299b8fd1a739fb5949664053708dff";
+    sha256 = "sha256-EnHRYX1/vXGKNuRqzPgtGaTkyKjZouF40+bn1KENjho=";
   };
 
   npmDepsHash = "sha256-3tt+k6ruIzDKeMTfXM6CkPpkCdwVawbOzFwCCxhdltQ=";
