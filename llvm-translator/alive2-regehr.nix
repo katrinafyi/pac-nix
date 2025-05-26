0c8e0e5ded987146169065e1e06251e30c554a60
rina <k@rina.fyi>
2025-05-26 16:02:06 +1000

    alive2-regehr: 0-unstable-2025-02-28 -> 0-unstable-2025-05-26
    Diff: https://github.com/regehr/alive2/compare/d0a6b4c8002e437cc6edbd4ccdac5c722cbc9c18...c2564d3c0a9771f8f37a2659ef1af55e770a0c67

--- llvm-translator/alive2-regehr.nix
+++ llvm-translator/alive2-regehr.nix
@@ -7,13 +7,13 @@
 
 (alive2-aslp.override { inherit llvmPackages; }).overrideAttrs (prev: {
   pname = "alive2-regehr";
-  version = "0-unstable-2025-02-28";
+  version = "0-unstable-2025-05-26";
 
   src = fetchFromGitHub {
     owner = "regehr";
     repo = "alive2";
-    rev = "d0a6b4c8002e437cc6edbd4ccdac5c722cbc9c18";
-    hash = "sha256-WW41gM4Uez0C/YgNqrzww3hejiqSCifBBKrgW8aqEi8=";
+    rev = "c2564d3c0a9771f8f37a2659ef1af55e770a0c67";
+    hash = "sha256-nXFYntQjkOMMEaX5fQQR3udHXqE5pxIauL51IatGKxQ=";
   };
 
 })
