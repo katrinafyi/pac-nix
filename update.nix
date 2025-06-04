39b496d0f6d66f151a6c29f4c596082fd0a06f1d
rina <k@rina.fyi>
2025-06-04 16:23:38 +1000

    updater: prefix to PATH instead of suffixing

--- update.nix
+++ update.nix
@@ -7,5 +7,5 @@
 let binpath = lib.makeBinPath [ nix nix-update ];
 in writers.writePython3Bin "pac-nix-update"
 { flakeIgnore = [ "E" "W" ]; }
-  (''import os; os.environ['PATH'] += os.pathsep + r"""${binpath}"""; del os'' +
+  (''import os; os.environ['PATH'] = r"""${binpath}""" + os.pathsep + os.environ['PATH']; del os'' +
   builtins.readFile ./update.py)
