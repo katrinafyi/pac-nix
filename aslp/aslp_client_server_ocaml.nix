1cd6ff1687571c49219520db24fe6026436f1a6c
rina <k@rina.fyi>
2026-05-29 14:47:44 +1000

    aslp_client_server_ocaml: 0.1.4-unstable-2026-02-09 -> 0.1.5-unstable-2026-05-29
    Diff: https://github.com/UQ-PAC/aslp-rpc/compare/001b2f954412466fce56c312a9258c3c4698ea6a...b3cd2aaf73771a3e43a4ee113c174f7fef238cb5

--- aslp/aslp_client_server_ocaml.nix
+++ aslp/aslp_client_server_ocaml.nix
@@ -4,27 +4,27 @@
   # ocamlPackages
 , lwt
 , mtime
-, aslp
 , aches
-, aslp_offline
+, aslp
+, aslp_lifter_ocaml
 }:
 
 buildDunePackage {
   pname = "aslp_client_server_ocaml";
-  version = "0.1.4-unstable-2026-02-09";
+  version = "0.1.5-unstable-2026-05-29";
 
   src = fetchFromGitHub {
     owner = "UQ-PAC";
     repo = "aslp-rpc";
-    rev = "001b2f954412466fce56c312a9258c3c4698ea6a";
-    hash = "sha256-AIAfLaTlXOU7QW2/Pk98pA6FoE0SJtzvXFsfmFCy6Ao=";
+    rev = "b3cd2aaf73771a3e43a4ee113c174f7fef238cb5";
+    hash = "sha256-3K/DiuYjj7B71GvzzJN/0e4wiCh15l8rbeVpDuk1ypk=";
   };
 
   checkInputs = [ ];
   nativeCheckInputs = [ ];
   buildInputs = [ ];
   nativeBuildInputs = [ ];
-  propagatedBuildInputs = [ aslp lwt mtime aches aslp_offline ];
+  propagatedBuildInputs = [ aslp lwt mtime aches aslp_lifter_ocaml ];
 
   doCheck = true;
 
