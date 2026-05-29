fd0891ac4986b92096b3c670b2d5fb01f0401f38
rina <k@rina.fyi>
2026-05-29 14:02:47 +1000

    aslp_lifter_ocaml: init

---
+++ aslp/aslp_lifter_ocaml.nix
@@ -0,0 +1,28 @@
+{ lib
+, fetchFromGitHub
+, buildDunePackage
+}:
+
+buildDunePackage {
+  pname = "aslp_lifter_ocaml";
+  version = "1.0.0";
+
+  minimalOCamlVersion = "4.14";
+
+  src = fetchFromGitHub {
+    owner = "UQ-PAC";
+    repo = "aslp-lifter-ocaml";
+    rev = "1.0.0";
+    hash = "sha256-T+Bqnytlasr+KP34rOWpg5dsdOfJKv7WM3j9cpmcj9s=";
+  };
+
+  doCheck = true;
+
+  outputs = [ "out" "dev" ];
+
+  meta = {
+    homepage = "https://github.com/UQ-PAC/aslp-lifter-ocaml";
+    description = " AArch64 offline lifter (pre-generated)";
+    maintainers = with lib.maintainers; [ katrinafyi ];
+  };
+}
